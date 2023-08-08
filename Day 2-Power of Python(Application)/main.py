78196d60ae94bcb938339dee3ee106bb57e63da1
Bainash <nischalbaidar@yahoo.com>
2023-08-08 18:28:49 +0545

    Day 2

---
+++ Day 2-Power of Python(Application)/main.py
@@ -0,0 +1,2 @@
+#some applications are jarvis, love calculator, Face Recognition, Flappy Bird game and Snake Game
+#Most of the Industry use python today for image recognition and machine learning, Web Development and many others
