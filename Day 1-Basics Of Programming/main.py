2d37385f4a6545f22c7de63c7f9e19e500e555e4
Bainash <nischalbaidar@yahoo.com>
2023-08-08 17:45:42 +0545

    Day 1

---
+++ Day 1-Basics Of Programming/main.py
@@ -0,0 +1,13 @@
+#print function prints the line
+print("Hello World")
+print(5)
+print(7+5)
+print("7+5") #prints 7+5 if in double quotes it is a string
+print('7+5') #it also prints 7+5 
+#There is no difference between single quotes and double quotes for defining string literals in Python
+
+print("He said, 'Hello!'")
+print('She replied, "Hi there!"')
+# Here strings can be printed as above where two quotation marks are used within each other
+
+
