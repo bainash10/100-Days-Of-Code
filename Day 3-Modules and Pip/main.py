09a0951d11816680f787fa71d016f80b4dd8afaa
Bainash <nischalbaidar@yahoo.com>
2023-08-09 17:53:36 +0545

    Added

--- Day 3-Modules and Pip/main.py
+++ Day 3-Modules and Pip/main.py
@@ -1,4 +1,7 @@
-import pandas
-import hashlib 
-import tensorflow
-print("Hi")
+import pandas #pandas already installed via powershell so it works
+import hashlib #grabbing/including builtin module
+# can check builtin module by writing 'builtin module in python' in GOOGLE
+print("Hi") #if not installed will show error in printing hi
+#import tensorflow 
+# if not installed tensorflow will show error can install by using pip install tensorflow either in terminal or in powershell
+#it is an external module
