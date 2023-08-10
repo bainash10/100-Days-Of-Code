aac68339f707eba714df5b22cf1c5e7eb910aca4
Bainash <nischalbaidar@yahoo.com>
2023-08-10 18:08:39 +0545

    Day 4

---
+++ Day 4-First Program/main.py
@@ -0,0 +1,24 @@
+#print(Hello World) 
+#wont run because to run in python need to give valid obj so to run need to give ""
+print(7)
+print("Hello World") #for string need to be in inverted comma ""
+print(5)
+print("Hello World", 7) #can print multiple values using comma
+print("Hello World",7, "This is nischal") #remember when used comma the space will be given for preceiding and receiding values in output
+print(5.0) 
+print(17*23)
+print("17*23")
+
+
+#This is quiz from tutorial.md
+#Poem of my own
+print("Twinkle twinkle little \nstar how I wonder what you are \t This is a poem from Nischal\n", "How is it guys?"+"\nLiked it or not?") #Here + concatenates the strings nee to specify space
+
+#NOTE:
+#This is single line Comment
+
+"""This is 
+multi
+line comment"""
+
+#In Python, both commas and the + operator can be used to combine values, but they have different behaviors and use cases.
