6240d9569bf9104195eb99cf14b104a2c5ea433e
Bainash <nischalbaidar@yahoo.com>
2023-08-13 16:43:12 +0545

    Day 7

---
+++ Day 7-Exercise 1-Calculator/exercise.py
@@ -0,0 +1,16 @@
+a=1
+b=2
+ans1=a+b
+print("The addition of",a,"and",b,"is",ans1)
+ans2=b-a
+print("The subtraction of",b,"and",a,"is",ans2)
+ans3=a*b
+print("The smultiplication of",a,"and",b,"is",ans3)
+ans4=a/b
+print("The division of",a,"and",b,"is",ans4)
+ans5=a//b
+print("The floor division of",a,"and",b,"is",ans5)
+ans6=a**b
+print("The exponential of",a,"and",b,"is",ans6)
+ans7=a%b
+print("The modulus of",a,"and",b,"is",ans7)
---
+++ Day 7-Exercise 1-Calculator/main.py
@@ -0,0 +1,11 @@
+print(5+6)
+print(5-6)
+print(5*6)
+print(5**6)  #exponential it means 5^6 i.e 5*5*5*5*5*5
+print(5/6) #division
+
+print(5//6) #floor division--> answer of 5/6 is 0.8333333 to take only 0 floor division is used
+#It is used to perform division between two numbers and returns the largest integer that is less than or equal to the result of the division.
+
+print(5%6)  #remainder --> modulus
+
