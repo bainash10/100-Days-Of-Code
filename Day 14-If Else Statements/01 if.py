33e67c677d92b756353f2ae3649e262fd37ca716
Bainash <nischalbaidar@yahoo.com>
2023-08-20 17:25:07 +0545

    Day 14

---
+++ Day 14-If Else Statements/01 if.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your age"))
+print("Your age is:",a)
+
+#conditional operators
+# >,<,>=,<=,==,!=
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+if(a>18):    
+    print("You can drive")  #here the space inside if statement is indentation i.e new block like curly braces in C programming which is specified as space in C Programming
+else:
+    print("You cannot drive")
