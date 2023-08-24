534fa663311b336cab21213ee50c745ce21f353d
Bainash <nischalbaidar@yahoo.com>
2023-08-24 16:17:12 +0545

    Day 17

---
+++ Day 17-For Loops/main.py
@@ -0,0 +1,22 @@
+name = 'Nischal'
+for i in name:
+    print(i)
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for x in colors:
+    print(x)
+
+for i in range(10): # 0<i<10
+    print(i)
+
+for i in range(2,9): # 2<i<9
+    print(i)
+
+for i in range(1, 12, 3): #this is step:3 that adds the pre outputted value with 3
+    print(i)
+
+# Note:
+# range(x,y,z)
+# where, x= start
+# y= stop
+# z= step
