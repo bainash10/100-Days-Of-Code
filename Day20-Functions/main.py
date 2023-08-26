788cd5797d6055553ba3b0750cdbbccd48d28d2f
Bainash <nischalbaidar@yahoo.com>
2023-08-26 16:07:37 +0545

    Day 20

---
+++ Day20-Functions/main.py
@@ -0,0 +1,23 @@
+def calculatesum(a,b):  #calculatesum is function name
+    sum = a+b
+    print(sum)
+
+def greater(a,b):
+    if(a>b):
+        print("First is greater")
+    else:
+        print("Second is greater or equal")
+
+def islesser(a,b):
+    pass   #wont process this function is used when to define funtion but body will be writtern by coder in future.
+
+a=8
+b=9
+c=10
+d=12
+
+calculatesum(a,b)
+greater(a,b)
+
+calculatesum(c,d)
+greater(c,d)
