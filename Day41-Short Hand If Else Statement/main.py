33dd8e85bbbc998299e09472e6c8992d27900a11
Bainash <nischalbaidar@yahoo.com>
2023-09-16 12:58:26 +0545

    Day 41

---
+++ Day41-Short Hand If Else Statement/main.py
@@ -0,0 +1,16 @@
+a = 3303
+b = 330
+#Short and simple to execute
+print("A") if a > b else print("=") if a == b else print("B")
+
+print(9) if a>b else ""
+
+c=8 if a>b else 0
+print(c)
+
+# result = value_if_true if condition else value_if_false 
+# -->syntax of short hand if else statement
+
+#Drawbacks:
+#It decreases readability of bigger projects
+
