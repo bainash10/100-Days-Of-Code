cec7e19c9ebb1ecabe38611c10eaec15df68d6b2
Bainash <nischalbaidar@yahoo.com>
2023-10-05 16:31:25 +0545

    Day 45

--- Day45-if name main/nischal.py
+++ Day45-if name main/nischal.py
@@ -5,4 +5,5 @@ def welcome():
 if __name__ == "__main__":
     welcome()   #__name__ indicates from where it is printing
 
+#It is important because to make sure not to run every functions of module if one function consist of deleting every file of your computer then it might be a cause of deleting every file
 
