c464a2312da551fb4d8c1c4c35ad2406344f4c51
Bainash <nischalbaidar@yahoo.com>
2023-10-05 16:29:39 +0545

    Day 45

---
+++ Day45-if name main/main.py
@@ -0,0 +1,3 @@
+import nischal  #only using import functions and variable can be executed
+nischal.welcome()  #prints twice as using import also prints so to solve:
+
---
+++ Day45-if name main/nischal.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("Welcome to function")
+
+# print(__name__)  #prints __main__
+if __name__ == "__main__":
+    welcome()   #__name__ indicates from where it is printing
+
+
