c84a19e25042deee219d63b6eb8ae94d6cd5aaa1
Bainash <nischalbaidar@yahoo.com>
2023-10-16 08:37:09 +0545

    Day 56

---
+++ Day56-Intro to OOPs/main.py
@@ -0,0 +1,21 @@
+#POP:
+def hello():
+    print("Hello")
+
+hello()
+
+#OOP: #if not used oop variable name is repeated like sales1, sales2, .....
+#Like in GTA, players all entity can be stored like health, equipments etc.
+sales1=8000
+profit1=2000
+ad1=1000
+# rajeev.sales1
+
+sales2=8000
+profit2=2000
+ad2=1000
+# sanjeev.sales1
+
+sales3=8000
+profit3=2000
+ad3=1000
