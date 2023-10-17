2976eb99018831c8c8cf816eff7e84800986471e
Bainash <nischalbaidar@yahoo.com>
2023-10-17 08:44:06 +0545

    Day 56"

--- Day56-Intro to OOPs/main.py
+++ Day56-Intro to OOPs/main.py
@@ -1,11 +1,9 @@
 #POP:
 def hello():
     print("Hello")
-
 hello()
 
-#OOP: #if not used oop variable name is repeated like sales1, sales2, .....
-#Like in GTA, players all entity can be stored like health, equipments etc.
+
 sales1=8000
 profit1=2000
 ad1=1000
@@ -19,3 +17,9 @@ ad2=1000
 sales3=8000
 profit3=2000
 ad3=1000
+
+#OOP:
+#if not used oop variable name is repeated like sales1, sales2, .....
+#Like in GTA, players all entity can be stored like health, equipments etc.
+#class is blueprint which is the form that will be filled out by harry, tom and shubham
+#objects are entity like harry, tom, shubham who fills railway form
