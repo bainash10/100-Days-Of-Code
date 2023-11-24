fff44ae1ce9afd88d1ea7922f6d6031ee76efc8b
Bainash <nischalbaidar@yahoo.com>
2023-11-24 14:10:10 +0545

    Day 87

---
+++ Day87-Shutil Module/main.py
@@ -0,0 +1,20 @@
+#this module is used to automate the processes
+#most useful module
+#to install we shouldnot use pip install shutil because it is built in module
+#used for doing high level file operation
+import shutil
+
+# shutil.copy("Day87-Shutil Module/main.py","Day87-Shutil Module/main2.py") #copy file main.py content to main2.py
+
+# shutil.copytree("Day87-Shutil Module/tutorial","Day87-Shutil Module/mytutorial")  #shutil.copytree used to copy the folder with the contents inside
+
+# shutil.move("Day87-Shutil Module/tutorial/test.txt","Day87-Shutil Module/test.txt")  #move file inside tutorial folder outside of tutorial folder with name test.txt
+
+# shutil.copytree("Day87-Shutil Module/tutorial","Day87-Shutil Module/mytutorial2")  created new folder
+
+shutil.rmtree("Day87-Shutil Module/mytutorial2")
+
+
+
+
+
---
+++ Day87-Shutil Module/main2.py
@@ -0,0 +1,6 @@
+#this module is used to automate the processes
+#most useful module
+#to install we shouldnot use pip install shutil because it is built in module
+#used for doing high level file operation
+import shutil
+shutil.copy("Day87-Shutil Module/main.py","Day87-Shutil Module/main2.py") #copy file main.py content to main2.py
