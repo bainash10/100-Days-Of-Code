7dcd0aaa35df8acc83414b3edebcc1dc72466aab
Bainash <nischalbaidar@yahoo.com>
2023-12-08 17:12:48 +0545

    Day100

---
+++ Day100-Conclusion and next destination/main.py
@@ -0,0 +1,11 @@
+#Thank you everyone !!!!!!
+#Where to go from here ??
+# -Do Projects
+# Remember machine learning and data science go hand in hand learn both
+#Pandas are used for data analysis in python
+#Remember the library and data analysis python programming is offering is just mind blowing, No javascript no C/CPP is offering this till date
+
+#Book -> Python for data analysis by Wes McKinney (Buy this anyhow)
+# Hands on Machine Learning with Scikit-Learn by Aureilien Geron --> always buy books of latest edition
+
+#Recommended not to data structure through python do through Java or CPP
